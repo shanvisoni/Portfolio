349b043a2201809eb906c900071652f3f4cbcbc6
shanvisoni <shanvisoni7388@gmail.com>
2025-01-28 20:36:49 +0530

    Shanvi's portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Aos from 'aos';
+import "aos/dist/aos.css"
 import './App.css';
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    Aos.init({
+      duration: 1000, 
+      once: false,    
+      mirror: false,  
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <>
+
+<Navbar/>
+<div className='container'>
+  <Home/>
+  <Skills/>
+  <Projects/>
+  <Contact/>
+</div>
+
+
+  </>
+  )
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+// .skills{
+//   width: 900px;
+// }
+
+// .skills .items{
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-wrap:wrap ;
+//   /* width: 700px; */
+//   margin: auto;
+//   /* background-color: rgb(34, 32, 32); */
+// }
+
+// .skills .items .item{
+// width: calc(33.33%);
+// background-color: rgb(34, 32, 32);
+// margin: 20px;
+// text-align: center;
+// padding:20px ;
+// border-radius: 10px;
+// border: 1px solid yellow;
+// box-shadow: var(--box-shadow);
+// }
+
+// .skills .items .item img{
+//   width: 70px;
+// }
---
+++ src/components/Skills.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import skills from './data/skills.json'
+const Skills = () => {
+  return (
+    <>
+     <div className='container skills' id='skills'>
+      <h1>skills</h1>
+      <div className='items'>
+      {skills.map((data)=>(<>
+        <div className='item'  key={data.id}
+           data-aos="flip-left"
+           data-aos-duration="1000"
+           >
+          {/* <img src={`/assets/${data.imageSrc}`} alt=""/> */}
+          <h3>{data.title}</h3>
+        </div>
+      </>))}
+      </div>
+    </div>
+    </>
+   
+  )
+}
+
+export default Skills
