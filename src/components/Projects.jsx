7e2ffbefdf98a7c894918bc51346473083df4f0a
shanvisoni <shanvisoni7388@gmail.com>
2025-04-26 23:33:57 +0530

    naming correction

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,7 +43,7 @@ const Projects = () => {
                     </StyledLink>
                   )}
                   <StyledLink to={data.source} className="btn btn-warning">
-                    Code
+                    Demo
                   </StyledLink>
                 </div>
               </div>
